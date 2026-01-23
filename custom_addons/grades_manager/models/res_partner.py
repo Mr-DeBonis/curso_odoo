321e4f588898e94c7d7b88279d02109204071479
Mr-DeBonis <ignacio.debonis7@gmail.com>
2026-01-23 11:02:11 -0300

    Add remove logic to teachers with unlink function

--- custom_addons/grades_manager/models/res_partner.py
+++ custom_addons/grades_manager/models/res_partner.py
@@ -8,3 +8,12 @@ class ResPartner(models.Model):
     is_freelance = fields.Boolean(string='Is freelance')
     is_student = fields.Boolean(string='Is student')
     vat = fields.Char(required=True)
+
+    def unlink(self):
+        for partner in self:
+            if partner.email == 'mainteacher@mail.com':
+                courses = self.env['grades.course'].search([('teacher_id','=', partner.id)])
+                secondary_teacher = self.env['res.partner'].search([('email','=','secundario@mail.com')])
+                courses.write({'teacher_id': secondary_teacher.id})
+        result = super(ResPartner, self).unlink()
+        return result
