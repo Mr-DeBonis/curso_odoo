652b7a4a9db368023719ed1498e725d6ca43e475
Mr-DeBonis <ignacio.debonis7@gmail.com>
2026-02-01 17:03:28 -0300

    Add copy logic to teachers and students

--- custom_addons/grades_manager/models/res_partner.py
+++ custom_addons/grades_manager/models/res_partner.py
@@ -7,13 +7,23 @@ class ResPartner(models.Model):
     is_teacher = fields.Boolean(string='Is Teacher')
     is_freelance = fields.Boolean(string='Is freelance')
     is_student = fields.Boolean(string='Is student')
-    vat = fields.Char(required=True)
+    vat = fields.Char(required=True, copy=False)
 
     def unlink(self):
         for partner in self:
             if partner.email == 'mainteacher@mail.com':
-                courses = self.env['grades.course'].search([('teacher_id','=', partner.id)])
-                secondary_teacher = self.env['res.partner'].search([('email','=','secundario@mail.com')])
+                courses = self.env['grades.course'].search([('teacher_id', '=', partner.id)])
+                secondary_teacher = self.env['res.partner'].search([('email', '=', 'secundario@mail.com')])
                 courses.write({'teacher_id': secondary_teacher.id})
         result = super(ResPartner, self).unlink()
         return result
+
+    def copy(self, default=None):
+        default = default or {}
+        if self.is_teacher:
+            default['name'] = 'Teacher'
+        if self.is_student:
+            default['name'] = 'Student'
+
+        res = super(ResPartner, self).copy(default)
+        return res
